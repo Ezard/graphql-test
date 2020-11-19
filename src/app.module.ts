7dc832e3c3c685ff4bf142d8955f1f644567b5cb
Ezard <ezard.ben@gmail.com>
2020-11-19 13:49:02 +0000

    Remove unused classes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MeResolver } from './me.resolver';
 
 @Module({
-  imports: [GraphQLModule.forRoot({
-    autoSchemaFile: 'schema.graphql'
-  })],
-  controllers: [AppController],
-  providers: [AppService, MeResolver],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.graphql',
+    }),
+  ],
+  controllers: [],
+  providers: [MeResolver],
 })
-export class AppModule {}
+export class AppModule {
+}
